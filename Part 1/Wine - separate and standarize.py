a5f1dbef6990d2f30ced133ee435908cfbcaed21
unaimendez <unai.mendez@estudiants.urv.cat>
2023-11-27 02:10:28 +0100

    wine add and standarized

---
+++ Part 1/Wine - separate and standarize.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+df = pd.read_csv('A2-wine/winequality-white.csv', sep=';')
+
+# Standardize the dataset
+scaler = preprocessing.StandardScaler().fit(df)
+df_scaled = scaler.transform(df)
+df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
+
+# Split the dataset into training and test sets
+train, test = train_test_split(df_scaled, test_size=0.2, random_state=42)
+
+# Save the training and test sets to files
+np.savetxt('A2-wine/wine-train.txt', train.values, fmt='%f')
+np.savetxt('A2-wine/wine-test.txt', test.values, fmt='%f')
