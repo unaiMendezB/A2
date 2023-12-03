ddfd886a73f8d430c2bd570e3c06f3e90f625ace
unaimendez <unai.mendez@estudiants.urv.cat>
2023-12-03 01:18:23 +0100

    spreading

--- Part 2/A2-bank/svm.py
+++ Part 2/A2-bank/svm.py
@@ -1,41 +1,43 @@
 import pandas as pd
 from sklearn import svm
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-# Load your training and test datasets
-df_train = pd.read_csv('bank-additional-train.csv')
-df_test = pd.read_csv('bank-additional-test.csv')
+# Load the datasets
+train_data = pd.read_csv('bank-additional-train.txt', sep=',', header=None)
+test_data = pd.read_csv('bank-additional-test.txt', sep=',', header=None)
 
-X_train = df_train.drop('y_yes', axis=1)
-y_train = df_train['y_yes']
-
-X_test = df_test.drop('y_yes', axis=1)
-y_test = df_test['y_yes']
-
-# Standardize the features
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# Split the datasets into features and labels
+X_train = train_data.iloc[:, :-1]
+y_train = train_data.iloc[:, -1]
+X_test = test_data.iloc[:, :-1]
+y_test = test_data.iloc[:, -1]
 
 # Define the parameter range for grid search
-param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001], 'kernel': ['linear', 'rbf']}
+param_grid = {'C': [0.1, 1, 10, 100, 1000],
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+              'kernel': ['rbf']}
 
-# Perform grid search with 5-fold cross-validation
-grid = GridSearchCV(svm.SVC(), param_grid, refit=True, verbose=2, cv=5)
+# Create a GridSearchCV object and fit it to the training data
+svc = svm.SVC()
+grid = GridSearchCV(svc, param_grid, refit=True, verbose=2, cv=10)
 grid.fit(X_train, y_train)
 
 # Print the best parameters
 print("Best parameters found: ", grid.best_params_)
 
-# Predict the labels of the test set
+# Predict the labels for the test set
 y_pred = grid.predict(X_test)
 
 # Compute the confusion matrix
 cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix: \n", cm)
 
 # Compute the classification error
-error = 100 * (cm[0][1] + cm[1][0]) / (cm[0][0] + cm[1][1] + cm[0][1] + cm[1][0])
-print("Classification Error (%): ", error)
+E = 100 * (cm[0][1] + cm[1][0]) / (cm[0][0] + cm[1][1] + cm[0][1] + cm[1][0])
+
+# Model Precision and Recall
+print(classification_report(y_test, y_pred))
+
+# Print the classification error and confusion matrix
+print("Classification error: ", E)
+print("Confusion matrix: \n", cm)
--- Part 2/A2-ring/svm.py
+++ Part 2/A2-ring/svm.py
@@ -14,11 +14,13 @@ X_test = test_data[['A', 'B']]
 y_test = test_data['Result']
 
 # Define the parameter range for grid search
-param_grid = [{'C': [0.1, 1, 10, 100, 1000], 'kernel': ['linear']}]
+param_grid = {'C': [100, 1000,10000],
+              'gamma': [0.01, 0.001],
+              'kernel': ['rbf']}
 
 # Create a GridSearchCV object and fit it to the training data
 svc = svm.SVC()
-grid = GridSearchCV(svc, param_grid, refit=True, verbose=2, cv=5)
+grid = GridSearchCV(svc, param_grid, refit=True, verbose=2, cv=20)
 grid.fit(X_train, y_train)
 
 # Print the best parameters
@@ -39,21 +41,3 @@ print(classification_report(y_test, y_pred))
 # Print the classification error and confusion matrix
 print("Classification error: ", E)
 print("Confusion matrix: \n", cm)
-
-'''
-# Predict the responses for test dataset
-y_pred = grid.predict(X_test)
-
-# Print the best parameters
-print("Best parameters found: ", grid.best_params_)
-
-# Model Accuracy
-print("Accuracy:", accuracy_score(y_test, y_pred))
-
-# Model Precision and Recall
-print(classification_report(y_test, y_pred))
-
-# Confusion Matrix
-print(confusion_matrix(y_test, y_pred))
-
-'''
